5bc50f3311d5b948941b4a50e07987629bd3d62e
injahu1 <injahu@hotmail.com>
2023-02-12 13:01:11 -0500

    agrego formulario funcional, creacion corresponsal

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Login from "./components/Login";
 import RetirarDinero from "./components/RetirarDinero";
+import FormNuevaCuentaCorresponsal from "./components/FormNuevaCuentaCorresponsal";
 import { Routes, Route, useNavigate, } from 'react-router-dom';
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
                     <Route path="/" element={<Login />} />
                     <Route path="/login" element={<Login />} />
                     <Route path="/retirardinero" element={<RetirarDinero />} />
+                    <Route path="/crear_corresponsal" element={<FormNuevaCuentaCorresponsal />} />
                 </Routes>
             }
         </>
